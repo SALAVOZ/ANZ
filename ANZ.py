db109f8d7b99a68b238c6b880654ebcc3e0a90c2
Абдулин Салават Раисович <Salavat.Abdulin@innostage-group.ru>
2022-11-08 18:47:49 +0300

    created js parsing

--- ANZ.py
+++ ANZ.py
@@ -1,7 +1,8 @@
 import argparse
-
 import requests
 import re
+from bs4 import BeautifulSoup
+
 
 class Wappalyzer:
     def __init__(self, host):
@@ -9,6 +10,8 @@ class Wappalyzer:
         self.schemas = []
         self.server = None
         self.js_frameworks = []
+        self.vulnerable = []
+
     def make_request(self, schema):
         try:
             r = requests.get(schema + '://' + self.host, verify=False)
@@ -20,7 +23,7 @@ class Wappalyzer:
             return None
 
     def get_schemas(self):
-        for schema in ['http', 'https']:
+        for schema in ['https']: # ПОТОМ СДЕЛАТЬ for schema in ['http', 'https']:
             response = self.make_request(schema)
             if response is not None:
                 self.schemas.append(schema)
@@ -50,6 +53,7 @@ class Wappalyzer:
             self.parse_framework(tag, schema)
 
     def parse_framework(self, path, schema):
+        version = None
         if 'http://' in path or 'https://' in path:
             try:
                 version = self.get_js_through_url(path)
@@ -66,7 +70,8 @@ class Wappalyzer:
             'technologie': technologie
             })
 
-    def get_js_through_url(self, url):
+    @staticmethod
+    def get_js_through_url(url):
         try:
             res = requests.get(url, verify=False)
             re_result = re.search(r'\d+?.\d+?.\d+?', res.request.url)
@@ -95,19 +100,101 @@ class Wappalyzer:
             print('Connection error: ' + schema + '://' + self.host + path)
             print('=' * 10)
 
-    def get_js_technologie(self, path):
+    @staticmethod
+    def get_js_technologie(path):
         try:
-            re_result = re.search(r'[/]?([a-zA-Z]+?).[-bundle.min@0-9]*?.js', path)
-            return re_result.group()
+            if 'http://' in path or 'https://' in path:
+                return None
+            else:
+                re_result = re.search(r'([a-zA-Z0-9]+)[-.@]+', path)
+                return re_result.group(1) # под индеком ноль почему-то re возвращает весь найденный
         except AttributeError:
             print('Error in get_js_technologie')
 
+    # СДЕЛАТЬ ПОЛУЧЕНИЕ ВЕРСИЙ ГДЕ @
     '''
     Накидываем CVE
     '''
     def parse_snyk(self):
         for fram in self.js_frameworks:
-            pass
+            if fram['technologie'] is not None and fram['version'] is not None:
+                tech = fram['technologie']
+                res = requests.get(f'https://security.snyk.io/package/npm/{tech}', verify=False)
+                soup = BeautifulSoup(res.text, 'lxml')
+                table = soup.find('table', {'id': 'sortable-table'})
+                if table is None:
+                    continue
+                trs = table.find_all('tr',class_='vue--table__row')
+                #spans = table.find_all('span',class_='vue--chip__value')
+                for tr in trs:
+                    spans = tr.find_all('span', class_='vue--chip__value')
+                    result = self.snyk_condition(fram['version'].replace('v', ''), spans)
+                    if result:
+                        vuln_obj = fram
+                        vuln_obj['vuln'] = []
+                        a = tr.find('a')['href']
+                        res = requests.get(f'https://security.snyk.io/{a}')
+                        soup = BeautifulSoup(res.text, 'lxml')
+                        div = soup.find('div', class_='vuln-info-block')
+                        spans = div.find_all('span')
+                        for span in spans:
+                            try:
+                                vuln = span.find('span').find('a').text
+                                vuln = re.search('([-WCVE0-9]+?\s*?\n)', vuln).group().replace('\n', '')
+                                vuln_obj['vuln'].append(vuln)
+                            except BaseException:
+                                continue
+                        self.vulnerable.append(vuln_obj)
+    '''
+    Уязвимо или нет
+    '''
+    @staticmethod
+    def snyk_condition(framework_version, spans):
+        result_all_spans = False
+        if framework_version is None:
+            return None
+        for span in spans:
+            result_one_span = True
+            version = span.text
+            condition = re.findall(r'[<>=]{1,2}\d+.\d+.\d+', version)
+            for condition in condition:
+                if '>=' in condition:
+                    if framework_version >= condition.replace('=', '').replace('>', '').replace('<', ''):
+                        result_one_span = result_one_span and True
+                        continue
+                    else:
+                        result_one_span = result_one_span and False
+                        continue
+                if '>' in condition:
+                    if framework_version > condition.replace('=', '').replace('>', '').replace('<', ''):
+                        result_one_span = result_one_span and True
+                        continue
+                    else:
+                        result_one_span = result_one_span and False
+                        continue
+                if '<=' in condition:
+                    if framework_version <= condition.replace('=', '').replace('>', '').replace('<', ''):
+                        result_one_span = result_one_span and True
+                        continue
+                    else:
+                        result_one_span = result_one_span and False
+                        continue
+                if '<' in condition:
+                    if framework_version < condition.replace('=', '').replace('>', '').replace('<', ''):
+                        result_one_span = result_one_span and True
+                        continue
+                    else:
+                        result_one_span = result_one_span and False
+                        continue
+                if '=' in condition:
+                    if framework_version == condition.replace('=', '').replace('>', '').replace('<', ''):
+                        result_one_span = result_one_span and True
+                        continue
+                    else:
+                        result_one_span = result_one_span and False
+                        continue
+            result_all_spans = result_all_spans or result_one_span
+        return result_all_spans
 
     def parse_nginx_site(self):
         pass
@@ -121,128 +208,7 @@ class Wappalyzer:
     '''
     def run(self):
         self.get_schemas()
-        print(self.js_frameworks)
-
-'''
-class Wappalyzer:
-    def __init__(self, ip):
-        self.ip = ip
-        self.https = True
-        self.http = True
-    def get_protocols(self, ip):
-        protocols = ['http', 'https']
-        list = []
-        for protocol in protocols:
-            try:
-                r = requests.get(protocol + '://' + ip, verify=False)
-                if r.status_code == 200:
-                    list.append(r)
-            except requests.exceptions.ConnectionError:
-                list.append(None)
-                if protocol == 'http':
-                    self.http = False
-                if protocol == 'https':
-                    self.https = False
-                continue
-        http_url = list[0]
-        https_url = list[1]
-        return http_url, https_url.url
-
-
-    def get_js_paths(self, response):
-        paths = []
-        text = response.text
-        indexs = []
-        n = 1
-        while True:
-            r = find_nth_occurence_in_string(text, 'script src=\"', n)
-            n += 1
-            if r != -1:
-                indexs.append(r)
-            else:
-                break
-        for index in indexs:
-            i = index
-            while text[i] != '\"':
-                i += 1
-            j = i + 1
-            while text[j] != '\"':
-                j += 1
-            paths.append(text[i:j].replace('\'', '').replace('\"', ''))
-        return paths
-
-
-    def build_url_to_get_js(self, list_of_paths):
-        list = []
-        if self.http:
-            for path in list_of_paths:
-                if 'https://' in path or 'http://' in path:
-                    list.append(path)
-                    continue
-                list.append('http://' + self.ip + '/' + path)
-
-        if self.https:
-            for path in list_of_paths:
-                if 'https://' in path or 'http://' in path:
-                    list.append(path)
-                    continue
-                list.append('https://' + self.ip + '/' + path)
-        return list
-
-    def get_js_versions(self, urls):
-        js = []
-        for url in urls:
-            res = requests.get(url, verify=False)
-            if not(self.ip in res.url):
-                result = self.analyze_js_file(res.url.split('/')[-1], res.text)
-                if result:
-                    js.append()
-                continue
-            framework = res.url.split(self.ip)[1]
-            if '/' in res.url:
-                result = self.analyze_js_file(framework.replace('js', '').replace('/', ''), res.text)
-            else:
-                result = self.analyze_js_file(framework, res.text)
-            if result:
-                js.append(result)
-        return js
-
-
-    Анализируем данный js файл на наличие версии(из названия файла и из содержимого файла)
-    axios.mim.js
-    vue.js
-    sweetalert2@9.js
-    jquery-3.2.1.min.js
-    bootstrap/jquery.min.js
-    signln.js?version=15
-    fontawesome/all.js
-
-    def analyze_js_file(self, name, text):
-        version = re.search('[0-9].[0-9].[0-9]', name)
-        if version:
-            return {'name': name, 'version': version}
-        version = re.search('v[0-9].[0-9]', text)
-        if version:
-            return {'name': name, 'version': version}
-        version = re.search('v[0-9].[0-9].[0-9]', text)
-        if version:
-            return {'name': name, 'version': version}
-
-
-    def make_request(self, url):
-        r = requests.get(url, verify=False)# Ставим verify=False, чтобы не было проблем с HTTPS
-        return r
-
-    def wappalyzer(self):
-        http_url, https_url = self.get_protocols(self.ip)
-        for url in [http_url, https_url]:
-            if url is None:
-                continue
-            self.get_js_versions(self.build_url_to_get_js(self.get_js_paths(self.make_request(url))))
-        return
-
-'''
-
+        print(self.vulnerable)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Write args: ')
